b66bbb5570544835708dabaa48d8ca71764a78de
Ozan <ozanbolel@outlook.com>
2020-07-23 23:35:38 +0300

    snippet scroll fixed in demo

--- demo/src/components/app/Snippet/Snippet.tsx
+++ demo/src/components/app/Snippet/Snippet.tsx
@@ -5,21 +5,19 @@ import css from "./Snippet.module.scss";
 const Snippet: React.FC<{ code: string; language?: Language; preview?: JSX.Element; margin?: boolean }> = ({ code, language = "jsx", preview, margin }) => {
   return (
     <div className={css.snippet + (margin ? " " + css.margin : "")}>
-      <div className={css.code}>
-        <Highlight {...defaultProps} code={code} language={language}>
-          {({ className, style, tokens, getLineProps, getTokenProps }) => (
-            <pre className={className} style={style}>
-              {tokens.map((line, i) => (
-                <div {...getLineProps({ line, key: i })}>
-                  {line.map((token, key) => (
-                    <span {...getTokenProps({ token, key })} />
-                  ))}
-                </div>
-              ))}
-            </pre>
-          )}
-        </Highlight>
-      </div>
+      <Highlight {...defaultProps} code={code} language={language}>
+        {({ style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={css.code} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
 
       {preview ? <div className={css.preview}>{preview}</div> : null}
     </div>
