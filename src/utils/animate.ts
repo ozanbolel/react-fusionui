a0fe2cb532af6835762ce84107762c2836a2b784
Ozan <ozanbolel@outlook.com>
2020-07-27 00:43:22 +0300

    easing function changed

--- src/utils/animate.ts
+++ src/utils/animate.ts
@@ -1,7 +1,7 @@
 const duration = 400;
 
-const easeInOutQuad = (progress: number) => {
-  return progress < 0.5 ? 2 * progress * progress : 1 - Math.pow(-2 * progress + 2, 2) / 2;
+const easeInOutSine = (x: number) => {
+  return -(Math.cos(Math.PI * x) - 1) / 2;
 };
 
 export const animate = (callback: (value: string) => any, config: { from: number; to: number; onRest?: Function }) => {
@@ -14,10 +14,11 @@ export const animate = (callback: (value: string) => any, config: { from: number
     const progress = (now - start) / duration;
 
     if (progress < 1) {
-      const value = ((to - from) * easeInOutQuad(progress) + from).toFixed(2);
+      const value = ((to - from) * easeInOutSine(progress) + from).toString();
       callback(value);
       window.requestAnimationFrame(step);
     } else {
+      callback(to.toString());
       if (onRest) onRest();
     }
   };
